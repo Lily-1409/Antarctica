e28a25deeb4923cfeb6f9024ff5c7b94b11ee0b3
Аникина Лилия <zolina.liliya@gmail.com>
2022-12-08 15:13:13 +0300

    added fixies

--- source/js/modules/mobile-menu.js
+++ source/js/modules/mobile-menu.js
@@ -1,24 +1,17 @@
 // открытие-закрытие мобильного меню
 const menuButton = document.querySelector('.main-nav-toogle');
-const svg = menuButton.querySelectorAll('svg');
-const iconOpen = menuButton.querySelector('.main-nav-toogle__iconOpen');
-const iconClose = menuButton.querySelector('.main-nav-toogle__iconClose');
 const nav = document.querySelector('.navigation-list');
 const header = document.querySelector('.header');
 
 menuButton.addEventListener('click', () => {
-  for (let i = 0; i < svg.length; i++) {
-    const currentIcon = svg[i];
 
-    currentIcon.classList.toggle('main-nav-toogle--hidden');
-
-    if (iconOpen.classList.contains('main-nav-toogle--hidden')) {
-      iconClose.style.display = 'block';
-      nav.classList.add('navigation-list--active');
-      header.classList.add('header-open-menu-js');
-    } else {
-      iconClose.style.display = 'none';
-      header.classList.remove('header-open-menu-js');
-    }
+  if (menuButton.classList.contains('main-nav-toogle--open-menu')) {
+    nav.classList.remove('navigation-list--active');
+    header.classList.remove('header-open-menu-js');
+    menuButton.classList.remove('main-nav-toogle--open-menu');
+  } else {
+    nav.classList.add('navigation-list--active');
+    header.classList.add('header-open-menu-js');
+    menuButton.classList.add('main-nav-toogle--open-menu');
   }
 });
--- source/js/modules/scroll.js
+++ source/js/modules/scroll.js
@@ -1,6 +1,7 @@
 // скролл до блоков
 const triggers = [].slice.call(document.querySelectorAll('.wrapper a[href^="#"]'));
-
+const header = document.querySelector('.header');
+const menuButton = document.querySelector('.main-nav-toogle');
 triggers.forEach((anchor) => {
   anchor.addEventListener('click', function (e) {
     e.preventDefault();
@@ -8,6 +9,9 @@ triggers.forEach((anchor) => {
     try {
       document.querySelector(e.target.getAttribute('href'))
           .scrollIntoView({behavior: 'smooth', block: 'start'});
+
+      header.classList.remove('header-open-menu-js');
+      menuButton.classList.remove('main-nav-toogle--open-menu');
     } catch (error) {
       //
     }
