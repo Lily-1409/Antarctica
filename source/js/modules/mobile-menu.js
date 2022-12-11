8403ccd0e4612538bf7028829f903f8c82f8aeca
Аникина Лилия <zolina.liliya@gmail.com>
2022-12-11 23:07:57 +0300

    added fixies -  mobile menu

--- source/js/modules/mobile-menu.js
+++ source/js/modules/mobile-menu.js
@@ -2,6 +2,10 @@
 const menuButton = document.querySelector('.main-nav-toogle');
 const nav = document.querySelector('.navigation-list');
 const header = document.querySelector('.header');
+const body = document.querySelector('body');
+const overlay = document.querySelector('.header-content');
+
+header.classList.add('header-close-menu-js');
 
 menuButton.addEventListener('click', () => {
 
@@ -9,9 +13,21 @@ menuButton.addEventListener('click', () => {
     nav.classList.remove('navigation-list--active');
     header.classList.remove('header-open-menu-js');
     menuButton.classList.remove('main-nav-toogle--open-menu');
+    body.classList.remove('lock');
   } else {
     nav.classList.add('navigation-list--active');
     header.classList.add('header-open-menu-js');
     menuButton.classList.add('main-nav-toogle--open-menu');
+    body.classList.add('lock');
+  }
+});
+
+document.addEventListener('click', (e) => {
+  const withinBoundaries = e.composedPath().includes(overlay);
+  if (!withinBoundaries) {
+    nav.classList.remove('navigation-list--active');
+    header.classList.remove('header-open-menu-js');
+    menuButton.classList.remove('main-nav-toogle--open-menu');
+    body.classList.remove('lock');
   }
 });
